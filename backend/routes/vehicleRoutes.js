8f35d9b3c5a5976bca743eb8eda42a365b125a4c
Nosheen Fatima <nosheenfatima025@gmail.com>
2026-05-04 08:32:26 +0500

    fix adminOnly import

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -1,11 +1,11 @@
-
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
+const { protect, adminOnly } = require("../middleware/authMiddleware"); // ✅ adminOnly add kiya
 const { addVehicle, getAllVehicles, getMyVehicles, deactivateVehicle } = require("../controllers/vehicleController");
+
+router.post("/add", protect, addVehicle);
 router.get("/all", protect, adminOnly, getAllVehicles);
 router.get("/my", protect, getMyVehicles);
-router.post("/add", addVehicle);
 router.put("/:id/deactivate", protect, deactivateVehicle);
 
 module.exports = router;
