7db6b72a11e294782a7783bc45636023e83a03d9
Nosheen Fatima <nosheenfatima025@gmail.com>
2026-05-04 08:22:42 +0500

    fix route protection

--- backend/routes/vehicleRoutes.js
+++ backend/routes/vehicleRoutes.js
@@ -3,10 +3,9 @@ const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 const { addVehicle, getAllVehicles, getMyVehicles, deactivateVehicle } = require("../controllers/vehicleController");
-
-router.post("/add", addVehicle);
-router.get("/all", getAllVehicles);
+router.get("/all", protect, adminOnly, getAllVehicles);
 router.get("/my", protect, getMyVehicles);
+router.post("/add", addVehicle);
 router.put("/:id/deactivate", protect, deactivateVehicle);
 
 module.exports = router;
--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 const { getWallet, rechargeWallet, getAllWallets, adminTopUp } = require("../controllers/walletController");
 
-router.get("/", protect, getWallet);
+router.get("/all", protect, adminOnly, getAllWallets);
 router.post("/recharge", protect, rechargeWallet);
+router.post("/topup", protect, adminOnly, adminTopUp);
 router.get("/all", getAllWallets);
-router.post("/topup", adminTopUp);
 
 module.exports = router;
