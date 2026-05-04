ecdaa9060cf4776d04f0f1a90a083601cc5ab0a2
Nosheen Fatima <nosheenfatima025@gmail.com>
2026-05-04 08:36:36 +0500

    fix adminOnly imports

--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -1,12 +1,11 @@
-
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
+const { protect, adminOnly } = require("../middleware/authMiddleware");
 const { getWallet, rechargeWallet, getAllWallets, adminTopUp } = require("../controllers/walletController");
 
+router.get("/", protect, getWallet);
 router.get("/all", protect, adminOnly, getAllWallets);
 router.post("/recharge", protect, rechargeWallet);
 router.post("/topup", protect, adminOnly, adminTopUp);
-router.get("/all", getAllWallets);
 
 module.exports = router;
